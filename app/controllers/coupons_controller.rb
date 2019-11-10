54b8a7980b392ee4440e30d6791e14b91bd9ace4
Randel Jiao <randeljiao@gmail.com>
2019-11-10 01:31:52 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -16,4 +16,22 @@ class CouponsController < ApplicationController
     @coupon.save
     redirect_to coupon_path(@coupon)
   end
+endclass CouponsController < ApplicationController
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new; end
+
+  def create
+    @coupon = Coupon.new
+    @coupon[:coupon_code] = params[:coupon][:coupon_code]
+    @coupon[:store] = params[:coupon][:store]
+    @coupon.save
+    redirect_to coupon_path(@coupon)
+  end
 end
